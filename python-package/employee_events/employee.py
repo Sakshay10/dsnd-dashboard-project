46d5c31d1e9ac4b914898c4d3af2da54682eeab7
Sakshay10 <sakshay1098@gmail.com>
2026-03-03 15:17:28 +0530

    Update employee.py

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,65 +1,56 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from .query_base import QueryBase
 
-# Import dependencies needed for sql execution
-# from the `sql_execution` module
-#### YOUR CODE HERE
-
-# Define a subclass of QueryBase
-# called Employee
-#### YOUR CODE HERE
-
-    # Set the class attribute `name`
-    # to the string "employee"
-    #### YOUR CODE HERE
+# Import pandas
+import pandas as pd
 
 
-    # Define a method called `names`
-    # that receives no arguments
-    # This method should return a list of tuples
-    # from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 3
-        # Write an SQL query
-        # that selects two columns 
-        # 1. The employee's full name
-        # 2. The employee's id
-        # This query should return the data
-        # for all employees in the database
-        #### YOUR CODE HERE
-    
+# Define a subclass of QueryBase called Employee
+class Employee(QueryBase):
 
-    # Define a method called `username`
-    # that receives an `id` argument
-    # This method should return a list of tuples
-    # from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 4
-        # Write an SQL query
-        # that selects an employees full name
-        # Use f-string formatting and a WHERE filter
-        # to only return the full name of the employee
-        # with an id equal to the id argument
-        #### YOUR CODE HERE
+    # Set class attribute
+    name = "employee"
 
 
-    # Below is method with an SQL query
-    # This SQL query generates the data needed for
-    # the machine learning model.
-    # Without editing the query, alter this method
-    # so when it is called, a pandas dataframe
-    # is returns containing the execution of
-    # the sql query
-    #### YOUR CODE HERE
+    # Method 1: Return all employee names and ids
+    def names(self):
+
+        query = """
+                SELECT first_name || ' ' || last_name AS full_name,
+                       employee_id
+                FROM employee
+                """
+
+        return self.execute_query(query)
+
+
+    # Method 2: Return full name for specific employee
+    def username(self, id):
+
+        query = f"""
+                SELECT first_name || ' ' || last_name AS full_name
+                FROM employee
+                WHERE employee_id = {id}
+                """
+
+        return self.execute_query(query)
+
+
+    # Method 3: Return ML model dataset as pandas DataFrame
     def model_data(self, id):
 
-        return f"""
-                    SELECT SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
+        query = f"""
+                    SELECT SUM(positive_events) positive_events,
+                           SUM(negative_events) negative_events
                     FROM {self.name}
                     JOIN employee_events
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+
+        data = self.execute_query(query)
+
+        # Convert to DataFrame
+        df = pd.DataFrame(data, columns=["positive_events", "negative_events"])
+
+        return df
